737476dd3209369d746d04ad402679bbeff49b1e
Pius <piuskri@gmail.com>
2021-05-08 15:07:42 +0200

    initial sucker

---
+++ src/test/java/com/svius/algotrader/AlgotraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svius.algotrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgotraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
